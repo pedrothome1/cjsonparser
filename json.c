f2aa20fa43a8c91138c99b1eeea4791ff3094580
pedrothome1 <pedrothome2@outlook.com>
2022-02-25 21:17:25 -0300

    parse objects

--- json.c
+++ json.c
@@ -1,31 +1,10 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 #include "json.h"
 
-#define BUF_SZ 100
-
-#define IN_WSP 0
-#define IN_OBJ 1
-#define IN_ARR 2
-#define IN_STR 3
-#define IN_KEY 4
-#define IN_VAL 5
-#define IN_NUM 6
-#define IN_BOL 7
-#define IN_NUL 8
-
-int where(int c) {
-  if (isspace(c))
-    return IN_WSP;
-  if (c == '{')
-    return IN_OBJ;
-  if (c == '[')
-    return IN_ARR;
-  if (c == '"')
-    return IN_STR;
-  return -1;
-}
+#define BUFSZ 100
 
 int isvalid_escape(int c) {
   switch (c) {
@@ -38,31 +17,44 @@ int isvalid_escape(int c) {
   }
 }
 
+int escape(int c) {
+  switch (c) {
+  case '\\': return '\\';
+  case 'b':  return '\b';
+  case 'f':  return '\f';
+  case 'n':  return '\n';
+  case 'r':  return '\r';
+  case 't':  return '\t';
+  case '"':  return '\"';
+  default:   return 0;
+  }
+}
+
 /* I'm not checking for the bounds of the buffers */
 int parse(const char *filename) {
   FILE *fp = fopen(filename, "r");
-  int c, in, retcode = -1; /* -1 is error */
-  char buf[BUF_SZ];
+  int c, retcode = -1; /* -1 is error */
   int b = 0;
+  char buf[BUFSZ];
 
   /* Here we begin to read the chars of the file */
-  /* If we are in white spaces, skip them */
-  while ((c = fgetc(fp)) != EOF) {
-    in = where(c);
+  /* Skip whitespaces */
+  while ((c = fgetc(fp)) != EOF && isspace(c))
+    ;
 
+  while (c != EOF) {
     /* We got to a {, so is probably an object */
-    if (in == IN_OBJ) {
+    if (c == '{') {
       /* Read what's inside the "object" */
-      while ((c = fgetc(fp)) != EOF) {
-        in = where(c);
-        
+
+      do {
         /* Skip whitespaces */
-        if (in == IN_WSP)
-          continue;
+        while ((c = fgetc(fp)) != EOF && isspace(c))
+          ;
 
         /* If we got to something that is not a double-quote, return with error */
         /* Because the first thing an object has inside is a quoted key */
-        if (in != IN_STR) {
+        if (c != '"') {
           goto END;
         }
 
@@ -75,6 +67,7 @@ int parse(const char *filename) {
             if (!isvalid_escape(c)) {
               goto END;
             }
+            c = escape(c);
           }
           else if (c == '"') {
             buf[b] = '\0';
@@ -106,6 +99,13 @@ int parse(const char *filename) {
         while ((c = fgetc(fp)) != EOF && isspace(c))
           ;
 
+        /* ======= Parsing the value ======= */
+        /*
+          Be careful so that `c` is consistent after parsing
+          any of the types. In this case, it will be the
+          character next to the last character of the value
+          being parsed.
+        */
         if (c == '"') {
           while ((c = fgetc(fp)) != EOF) {
             if (c == '\\') {
@@ -113,6 +113,7 @@ int parse(const char *filename) {
               if (!isvalid_escape(c)) {
                 goto END;
               }
+              c = escape(c);
             }
             else if (c == '"') {
               buf[b] = '\0';
@@ -120,23 +121,68 @@ int parse(const char *filename) {
             }
             buf[b++] = c;
           }
+          c = fgetc(fp);
+          printf("Value: %s\n\n", buf);
         }
+        /* Warning: Potential bug */
         else if (isdigit(c)) {
+          buf[b++] = c;
+          /* We are not really validating the number */
+          /* If it's not valid will be 0.0 because of atof */
           while ((c = fgetc(fp)) != EOF) {
-            
+            if (isdigit(c) || c == '.') {
+              buf[b++] = c;
+            }
+            else {  /* May be a comma or a whitespace */
+              buf[b] = '\0';
+              break;
+            }
           }
+          double num = atof(buf);
+          printf("Value: %f\n\n", num);
         }
         else if (c == 'n') {  /* null */
-
+          buf[b++] = c;
+          buf[b++] = fgetc(fp);
+          buf[b++] = fgetc(fp);
+          buf[b++] = fgetc(fp);
+          buf[b] = '\0';
+          if (strcmp(buf, "null") != 0)
+            goto END;
+          c = fgetc(fp);
+          printf("Value: %s\n\n", buf);
         }
         else if (c == 't') {  /* true */
-
+          buf[b++] = c;
+          buf[b++] = fgetc(fp);
+          buf[b++] = fgetc(fp);
+          buf[b++] = fgetc(fp);
+          buf[b] = '\0';
+          if (strcmp(buf, "true") != 0)
+            goto END;
+          c = fgetc(fp);
+          printf("Value: %s\n\n", buf);
         }
         else if (c == 'f') {  /* false */
-
+          buf[b++] = c;
+          buf[b++] = fgetc(fp);
+          buf[b++] = fgetc(fp);
+          buf[b++] = fgetc(fp);
+          buf[b++] = fgetc(fp);
+          buf[b] = '\0';
+          if (strcmp(buf, "false") != 0)
+            goto END;
+          c = fgetc(fp);
+          printf("Value: %s\n\n", buf);
         }
 
-      }
+        b = 0;  /* flush buffer */
+
+        /* Skip whitespaces and closing curly braces */
+        while (isspace(c) || c == '}')
+          c = fgetc(fp);
+
+      } while (c == ',');
     }
   }
 
